faa9aee5a74a11f5e1fe485a44c3341d4a4d6368
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-07-15 11:42:48 +0200

    adding navBar

---
+++ resources/views/navBar.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Company</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="/">Company</a>
+        </div>
+        <ul class="nav navbar-nav">
+            <li><a href="/branchOffice/list/">List branch offices</a></li>
+            <li><a href="/employees/list/">List employees</a></li>
+            <li><a href="/products/list/">List products</a></li>
+        </ul>
+    </div>
+</nav>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
