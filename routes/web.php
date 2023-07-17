d2c3dbf1be8874d93cc767b8ef8a92c5e51d1e1c
zabukovnikmaj <maj.zabukovnik@aspiria.com>
2023-07-17 13:50:44 +0200

    adding routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [Controllers\Controller::class, 'index']);
+
+Route::prefix('branchOffice')->group(function () {
+    Route::get('/list', [Controllers\BranchOfficeController::class, 'list']);
+    Route::get('/create', [Controllers\BranchOfficeController::class, 'edit']);
+    Route::post('/create', [Controllers\BranchOfficeController::class, 'save']);
+    Route::get('/edit/{branchOffice}', [Controllers\BranchOfficeController::class, 'edit']);
+    Route::put('/edit/{branchOffice}', [Controllers\BranchOfficeController::class, 'update']);
+    Route::delete('/delete/{branchOffice}', [Controllers\BranchOfficeController::class, 'delete']);
+});
+
+Route::prefix('products')->group(function () {
+    Route::get('/list', [Controllers\ProductsController::class, 'list']);
+    Route::get('/create', [Controllers\ProductsController::class, 'edit']);
+    Route::post('/create', [Controllers\ProductsController::class, 'save']);
+    Route::get('/edit/{employees}', [Controllers\ProductsController::class, 'edit']);
+    Route::put('/edit/{employees}', [Controllers\ProductsController::class, 'update']);
+    Route::delete('/delete/{employees}', [Controllers\ProductsController::class, 'delete']);
+});
+
+Route::prefix('employees')->group(function () {
+    Route::get('/list', [Controllers\EmployeesController::class, 'list']);
+    Route::get('/create', [Controllers\EmployeesController::class, 'edit']);
+    Route::post('/create', [Controllers\EmployeesController::class, 'save']);
+    Route::get('/edit/{employees}', [Controllers\EmployeesController::class, 'edit']);
+    Route::put('/edit/{employees}', [Controllers\EmployeesController::class, 'update']);
+    Route::delete('/delete/{employees}', [Controllers\EmployeesController::class, 'delete']);
 });
